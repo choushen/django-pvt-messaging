fb46a5a2b4e641e85ce56e3e1997a34a7215e0a6
choushen <jacob.mckenzie@icloud.com>
2024-07-13 20:16:52 +0100

    Added the messenger models to the wrong admins.py (amending in this push)

--- django_pvt_messaging/messenger/admin.py
+++ django_pvt_messaging/messenger/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Connection, Message, Notification
+
+admin.site.register(Connection)
+admin.site.register(Message)
+admin.site.register(Notification)
